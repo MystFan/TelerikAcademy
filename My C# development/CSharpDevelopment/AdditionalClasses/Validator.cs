ae9ddccec43c29cfc83905b86316ab613fa39797
toshko <toshkooooooooooo@gmail.com>
2015-04-11 20:29:17 +0300

    commit Validator class

---
+++ My C# development/CSharpDevelopment/AdditionalClasses/Validator.cs
@@ -0,0 +1,32 @@
+﻿
+namespace AdditionalClasses
+{
+    using System;
+
+    public class Validator
+    {
+        public static void CheckStringIsNullOrEmpty(string value, string message = "")
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(message);
+            }
+        }
+
+        public static void CheckObjectIsNull(object obj, string message = "")
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(message);
+            }
+        }
+
+        public static void CheckValueIsNegativ<T>(T value, string message = "") where T : struct,IComparable
+        {
+            if (value.CompareTo(default(T)) < 0)
+            {
+                throw new ArgumentOutOfRangeException(message);
+            }
+        }
+    }
+}
