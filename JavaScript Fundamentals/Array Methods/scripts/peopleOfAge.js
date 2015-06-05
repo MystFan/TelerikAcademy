a7a694770d4758b2bfd0759256c7d3068e604d38
toshko <toshkooooooooooo@gmail.com>
2015-06-05 13:17:44 +0300

    fix method use

--- JavaScript Fundamentals/Array Methods/scripts/peopleOfAge.js
+++ JavaScript Fundamentals/Array Methods/scripts/peopleOfAge.js
@@ -5,11 +5,11 @@
 //Write a function that checks if an array of person contains only people of age (with age 18 or greater)
 //    Use only array methods and no regular loops (for, while)
 
-var peopleOfAge = persons.filter(function (person) {
+var isAllAdults = persons.every(function (person) {
     return person.age >= 18;
 });
 
 jsConsole.writeLine("Problem 2:");
-jsConsole.writeLine(peopleOfAge.join(', '));
+jsConsole.writeLine(isAllAdults ? 'TRUE' : 'FALSE');
 console.log("Problem 2:");
-console.log(peopleOfAge);
+console.log(isAllAdults);
