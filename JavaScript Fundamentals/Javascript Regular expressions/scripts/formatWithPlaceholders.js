42bcafe82aba4e26b15ed431db0bf39776c6421e
toshko <toshkooooooooooo@gmail.com>
2015-06-06 20:24:53 +0300

    Javascript Regular expressions homework

---
+++ JavaScript Fundamentals/Javascript Regular expressions/scripts/formatWithPlaceholders.js
@@ -0,0 +1,27 @@
+﻿//Problem 1. Format with placeholders
+
+//    Write a function that formats a string. The function should have placeholders, as shown in the example
+//        Add the function to the String.prototype
+
+if (!String.prototype.format) {
+    String.prototype.format = function (options) {
+        if((!(options instanceof Object)) || Array.isArray(options)){
+            throw 'Parameter must be of type object';
+        }
+        var i, len, pattern,
+            result = this,
+            keys = Object.keys(options);
+
+        for (i = 0, len = keys.length; i < len; i+=1) {
+            pattern = new RegExp('#{' + keys[i] + '}', 'g');
+            result = result.replace(pattern, options[keys[i]]);
+        }
+        return result;
+    };
+}
+
+console.log('Problem 1:');
+var firstOptions = { name: 'John' };
+console.log('Hello, there! Are you #{name}?'.format(firstOptions));
+var secondOptions = { name: 'John', age: 13 };
+console.log('My name is #{name} and I am #{age}-years-old'.format(secondOptions));
---
+++ JavaScript Fundamentals/Javascript Regular expressions/scripts/htmlBinding.js
@@ -0,0 +1,62 @@
+﻿//Problem 2. HTML binding
+
+//Write a function that puts the value of an object into the content/attributes of HTML tags.
+//    Add the function to the String.prototype
+
+if (!String.prototype.bind) {
+    String.prototype.bind = function (str, options) {
+        if (typeof(str) !== 'string') {
+            throw 'First parameter must be of type string';
+        }
+        if ((!(options instanceof Object)) || Array.isArray(options)) {
+            throw 'Second parameter must be of type object';
+        }
+
+        var i,j, length, len, attributePattern, valuePattern, keyPattern, attributeValueString, keyValueString, currentTagString, tagsRegex, tags, attributes,
+            result = this,
+            keys = Object.keys(options);
+
+        attributePattern = /data-bind-[A-z]*="[A-z]*"/g;
+        valuePattern = /"[A-z]*"/g;
+        keyPattern = /-[A-z]*=/g;
+        tagsRegex = /<([a-z]+)*[^/]*?><\/(.?)>|<[A-z]*[^/]*?><\/[A-z]*>/g ;
+        tags = result.match(tagsRegex);
+
+        for (j = 0, length = tags.length; j < length; j += 1) {
+            attributes = tags[j].match(attributePattern);
+            currentTagString = tags[j];
+
+            for (i = 0, len = attributes.length; i < len; i += 1) {
+                attributeValueString = attributes[i].match(valuePattern)[0];
+                attributeValueString = attributeValueString.substring(1, attributeValueString.length - 1);
+
+                keyValueString = attributes[i].match(keyPattern)[0];
+                keyValueString = keyValueString.substring(1, keyValueString.length - 1);
+
+                if (options.hasOwnProperty(attributeValueString)) {
+                    switch (keyValueString) {
+                        case 'content': tags[j] = tags[j].replace(/>/, '>' + options[attributeValueString]);
+                            break;
+                        case 'href': tags[j] = tags[j].replace(/>/, ' href="' + options[attributeValueString] + '">');
+                            break;
+                        case 'class': tags[j] = tags[j].replace(/>/, ' class="' + options[attributeValueString] + '">');
+                            break;
+                    }
+                }
+            }
+
+            result = result.replace(currentTagString, tags[j]);
+        }
+        
+
+
+        return result;
+    }
+}
+
+console.log('Problem 2:')
+var example1 = '<div data-bind-content="name"></div>';
+console.log(example1.bind('', { name: 'Steven' }));
+
+var example2 = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></а>'
+console.log(example2.bind('', { name: 'Elena', link: 'http://telerikacademy.com' }));
