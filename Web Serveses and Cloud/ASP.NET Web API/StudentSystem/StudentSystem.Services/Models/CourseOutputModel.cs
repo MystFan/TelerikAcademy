c5c6d7b9ba3ef82c9a1e0ded60bede271f0bd588
toshko <toshkooooooooooo@gmail.com>
2015-10-29 14:36:24 +0200

    Add ASP.NET Web API Homework

---
+++ Web Serveses and Cloud/ASP.NET Web API/StudentSystem/StudentSystem.Services/Models/CourseOutputModel.cs
@@ -0,0 +1,18 @@
+﻿using StudentSystem.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentSystem.Services.Models
+{
+    public class CourseOutputModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<string> Materials { get; set; }
+
+        public IEnumerable<StudentOutputModel> Students { get; set; }
+    }
+}
